be78ee985cf66ee9ba9686e7a7823e3e0714ac88
Diego Pereira <rockin.jack@gmail.com>
2016-08-07 22:43:18 -0300

    feat: added docopt cmd line parameters

--- client-server/client.py
+++ client-server/client.py
@@ -1,28 +1,52 @@
+"""
+client
+
+Usage:
+    client [(-a | --addresses) <addresses>...]
+
+Options:
+    -a --addresses  A list of space separated addresses the client will connect to.
+    -h --help   Prints this help message.
+"""
+
 import time
 import uuid
 import zmq
 
+from docopt import docopt
 
-client_id = uuid.uuid1()
 
-ctx = zmq.Context()
+def run_client(arguments):
+    address_argument = arguments.get('<addresses>')
+    if address_argument:
+        addresses = ['tcp://' +  a for a in arguments.get('<addresses>')]
+    else:
+        addresses = ['tcp://127.0.0.1:8080', 'tcp://127.0.0.1:8081']
 
-socket = ctx.socket(zmq.XREQ)
-socket.SNDHWM = 1
-socket.connect("tcp://127.0.0.1:8080")
-socket.connect("tcp://127.0.0.1:8081")
+    client_id = uuid.uuid1()
 
-poller = zmq.Poller()
-poller.register(socket, zmq.POLLIN)
+    ctx = zmq.Context()
 
-while True:
-    try:
-        socket.send(str(client_id), zmq.NOBLOCK)
-        state = dict(poller.poll(50))
-        if state.get(socket) == zmq.POLLIN:
-            print socket.recv()
-        else:
-            print "Timed out, skipping."
-    except zmq.ZMQError as e:
-        print "Failed with {}, skipping.".format(e)
-    time.sleep(.3)
+    socket = ctx.socket(zmq.XREQ)
+    socket.SNDHWM = 1
+
+    for address in addresses:
+        socket.connect(address)
+
+    poller = zmq.Poller()
+    poller.register(socket, zmq.POLLIN)
+
+    while True:
+        try:
+            socket.send(str(client_id), zmq.NOBLOCK)
+            state = dict(poller.poll(50))
+            if state.get(socket) == zmq.POLLIN:
+                print socket.recv()
+            else:
+                print "Timed out, skipping."
+        except zmq.ZMQError as e:
+            print "Failed with {}, skipping.".format(e)
+        time.sleep(.3)
+
+if __name__ == '__main__':
+    run_client(docopt(__doc__))
--- client-server/server.py
+++ client-server/server.py
@@ -1,18 +1,38 @@
+"""
+server
+
+Usage:
+    server [(-p | --port) <port>]
+    server (-h | --help)
+
+Options:
+    -p --port   Sets the tcp port the server will bind to. Defaults to 8080.
+    -h --help   Shows this help messsage.
+"""
+
 import sys
 import zmq
 
+from docopt import docopt
+
 from helpers.throughput import Counter
 
-ctx = zmq.Context()
-socket = ctx.socket(zmq.XREP)
-socket.bind("tcp://0.0.0.0:8080")
 
-counter = Counter()
+def run_server(arguments):
+    port = arguments.get('<port>') or '8080'
+    ctx = zmq.Context()
+    socket = ctx.socket(zmq.XREP)
+    socket.bind("tcp://0.0.0.0:{}".format(port))
 
-try: 
-    while True:
-        msg = socket.recv_multipart()
-        socket.send_multipart(msg)
-        counter.inc()
-except KeyboardInterrupt:
-    counter.stop()
+    counter = Counter()
+
+    try: 
+        while True:
+            msg = socket.recv_multipart()
+            socket.send_multipart(msg)
+            counter.inc()
+    except KeyboardInterrupt:
+        counter.stop()
+
+if __name__ == "__main__":
+    run_server(docopt(__doc__))
